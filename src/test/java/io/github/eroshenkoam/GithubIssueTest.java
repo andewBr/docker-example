24786549c9b0da2b2e16cea85077ba12eb50315d
Andrew <burov.andrew@gmail.com>
2021-05-24 09:49:23 +0300

    first2

--- src/test/java/io/github/eroshenkoam/GithubIssueTest.java
+++ src/test/java/io/github/eroshenkoam/GithubIssueTest.java
@@ -41,7 +41,7 @@ public class GithubIssueTest {
         capabilities.setAcceptInsecureCerts(true);
 
         WebDriver driver = new RemoteWebDriver(
-                new URL("http://127.0.0.1:4444/wd/hub"),
+                new URL("http://192.168.0.16:4444/wd/hub"),
                 capabilities
         );
         driver.manage().window().maximize();
